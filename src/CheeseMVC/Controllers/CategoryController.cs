ff0f647669d153d7f544df10868b5ea327bbd1ae
Malcom King <kingmalcom@rocketmail.com>
2018-12-16 16:15:53 -0600

    unfinish part 1

---
+++ src/CheeseMVC/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CheeseMVC.Data;
+using CheeseMVC.Models;
+using CheeseMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CheeseMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CheeseDbContext context;
+
+        public CategoryController(CheeseDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            List<CheeseCategory> categoriesList = context.Categories.ToList();
+            
+            return View(categoriesList);
+        }
+
+        public IActionResult Add()
+        {
+            AddCategoryViewModel addCategoryViewModel = new AddCategoryViewModel();
+            return View(addCategoryViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddCategoryViewModel addCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                CheeseCategory newCategory = new CheeseCategory();
+                newCategory.Name = addCategory.Name;
+                context.Categories.Add(newCategory);
+                context.SaveChanges();
+
+                return Redirect("Index");
+            }
+            else
+            {
+                return View(addCategory);
+            }
+
+        }
+    }
+}
